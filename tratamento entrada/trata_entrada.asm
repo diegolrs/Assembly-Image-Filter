96aa6ea5d495bf105c401f58c3a7af2d429e2b36
Diego Reis <diegoluis200302@gmail.com>
2022-11-22 15:28:05 -0300

    filtering inputs

--- tratamento entrada/trata_entrada.asm
+++ tratamento entrada/trata_entrada.asm
@@ -10,10 +10,6 @@ includelib \masm32\lib\masm32.lib
 
 include remove_cr.inc
 
-;-------------------------------
-;Escreva um programa que some 7 ao numero inserido
-;-------------------------------
-
 .data
 inputString db 50 dup(0)
 outputString db 50 dup(0)
@@ -31,68 +27,93 @@ tamanho_msg dd 0 ; Variavel para armazenar tamanho da string de mensagem termina
 
 ;Variaveis ----------------------------
 console_write_count dd 0 ; Variavel para armazenar caracteres lidos/escritos na console
-nome_do_arquivo db 50 dup(0)
-
-
 outputHandle dd 0 ; Variavel para armazenar o handle de saida
 write_count dd 0; Variavel para armazenar caracteres escritos na console
 
+
+;Usados
+inputInteger db 50 dup(0)
+
+;Variaveis
+nome_do_arquivo db 50 dup(0)
+index_selecionado dd 0H
+valor_para_adicionar dd 0H
+
+
 .code
+
+_RequestInteger:
+    ;Prologo da subrotina --------
+    push ebp
+    mov ebp, esp
+    
+    ;Faz leitura do console
+    invoke ReadConsole, inputHandle, addr inputInteger, sizeof inputInteger, addr console_count, NULL
+
+    ;Remove enter
+    push offset inputInteger
+    call _RemoveCarriageReturn
+
+    ;Salva conversao em EAX
+    invoke atodw, addr inputInteger ; deixa inteiro convertido em eax
+
+    ;Epilogo da subrotina --------   
+    mov esp, ebp
+    pop ebp
+    ret
+    
 start:
     ;CONFIGURANDO STREAMS -----
-    INVOKE GetStdHandle, STD_INPUT_HANDLE
-    MOV inputHandle, eax
-    INVOKE GetStdHandle, STD_OUTPUT_HANDLE
-    MOV outputHandle, eax  
+    invoke GetStdHandle, STD_INPUT_HANDLE
+    mov inputHandle, eax
+    invoke GetStdHandle, STD_OUTPUT_HANDLE
+    mov outputHandle, eax  
+    ; -------------
 
-    ;Solicitar nome do arquivo de saída ------------
-    mov console_write_count, 0
-    invoke StrLen, addr msg_nome_arquivo
-    mov tamanho_msg, eax
-    invoke WriteConsole, outputHandle, addr msg_nome_arquivo, tamanho_msg, addr console_write_count, NULL
+    ;Solicita nome do arquivo -----------------
+        ;---- Printa mensagem ----
+        invoke StrLen, addr msg_nome_arquivo
+        invoke WriteConsole, outputHandle, addr msg_nome_arquivo, eax, addr console_write_count, NULL
+
+        ;---- Aguarda e trata entrada ----
+        invoke ReadConsole, inputHandle, addr nome_do_arquivo, sizeof nome_do_arquivo, addr console_count, NULL
+        push offset nome_do_arquivo
+        call _RemoveCarriageReturn
+
+    ;Solicitar o index da cor ------------------
+        ;---- Printa mensagem ----
+        invoke StrLen, addr msg_index_cor
+        invoke WriteConsole, outputHandle, addr msg_index_cor, eax, addr console_write_count, NULL
+
+        ;---- Aguarda e trata entrada ----
+        call _RequestInteger
+        mov index_selecionado, eax
+
+    ;Solicitar valor para adicionar ------------
+        ;---- Printa mensagem ----
+        invoke StrLen, addr msg_qtd_add
+        invoke WriteConsole, outputHandle, addr msg_qtd_add, eax, addr console_write_count, NULL
+
+        ;---- Aguarda e trata entrada ----
+        call _RequestInteger
+        mov valor_para_adicionar, eax
 
 
-    ;LENDO STRING ----
-    INVOKE ReadConsole, inputHandle, addr nome_do_arquivo, sizeof nome_do_arquivo, addr console_count, NULL
-
-    push offset nome_do_arquivo
-    call _RemoveCarriageReturn
-        ;Escrevendo string --------------
+    ;Escrevendo strings --------------
     INVOKE StrLen, addr nome_do_arquivo
     MOV tamanho_out_string, eax
     INVOKE WriteConsole, outputHandle, addr nome_do_arquivo, tamanho_out_string, addr console_count, NULL
+
+    mov eax, index_selecionado
+    invoke dwtoa, eax, addr inputInteger
+    INVOKE StrLen, addr inputInteger
+    INVOKE WriteConsole, outputHandle, addr inputInteger, eax, addr console_count, NULL
+
+    mov eax, valor_para_adicionar
+    invoke dwtoa, eax, addr inputInteger
+    INVOKE StrLen, addr inputInteger
+    INVOKE WriteConsole, outputHandle, addr inputInteger, eax, addr console_count, NULL
     ;----------------------------
 
-    ;LENDO INPUT INTEIRA ----
-    INVOKE ReadConsole, inputHandle, addr inputString, sizeof inputString, addr console_count, NULL
-    INVOKE StrLen, addr inputString
-    MOV tamanho_string, eax
-
-    push offset inputString
-    call _RemoveCarriageReturn
-
-    INVOKE atodw, addr inputString ; deixa inteiro convertido em eax
-    MOV integer1, eax
-    ADD integer1, 7
-    MOV EAX, integer1
-    INVOKE dwtoa, EAX, addr outputString
-    INVOKE StrLen, addr outputString
-    MOV tamanho_out_string, eax
-    INVOKE WriteConsole, outputHandle, addr outputString, tamanho_out_string, addr console_count, NULL
-    ;----------------------------
-
-    ;Solicitar o index da cor ------------
-    mov console_write_count, 0
-    invoke StrLen, addr msg_index_cor
-    mov tamanho_msg, eax
-    invoke WriteConsole, outputHandle, addr msg_index_cor, tamanho_msg, addr console_write_count, NULL
-
-    ;Solicitar a quantidade a adicionar na banda ------------
-    mov console_write_count, 0
-    invoke StrLen, addr msg_qtd_add
-    mov tamanho_msg, eax
-    invoke WriteConsole, outputHandle, addr msg_qtd_add, tamanho_msg, addr console_write_count, NULL
-
-    FINALIZA:
     INVOKE ExitProcess, 0
 end start
