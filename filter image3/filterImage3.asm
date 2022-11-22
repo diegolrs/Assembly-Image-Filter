ccf536d30da3e47ba8d135fc361957dd5f41d2fe
Diego Reis <diegoluis200302@gmail.com>
2022-11-22 19:18:52 -0300

    filtro final

--- filter image3/filterImage3.asm
+++ filter image3/filterImage3.asm
@@ -13,16 +13,16 @@ include \masm32\macros\macros.asm
 
 ;Bibliotecas criadas 
 include clamp.inc
+include console-messages.inc
+include myIO.inc
 
 .data
     ;Read file variables
     original_fileHandle dd 0H
-    original_fileName db "catita.bmp", 0H
     original_readCount dd 0H
 
     ;Write file variables
     copy_fileHandle dd 0H
-    copy_fileName db "catita2.bmp", 0H
     copy_writeCount dd 0H
 
     ;File Buffers
@@ -31,9 +31,11 @@ include clamp.inc
     bgr_color_buffer byte 3 DUP(0)
     bgr_color_ptr dd OFFSET bgr_color_buffer
     
-    ;Filter variables
+    ;Core variables
     color_index dd 1H
     value_to_add dd 0H
+    copy_name db 50 dup(0)
+    originalfile_name db 50 dup(0)
 
 .code   
     ;Copia primeiros 54 bytes da imagem
@@ -51,7 +53,7 @@ include clamp.inc
         ;Epilogo da subrotina --------
         mov esp, ebp
         pop ebp
-        ret ; return void
+        ret; return void
 
 
     ;Filtra pixel
@@ -88,7 +90,7 @@ include clamp.inc
         push eax
 
 
-        ;----- Pegando valor no index para clampar -------
+        ;----- Pegando valor na banda (index) desejada -------
 
         cmp DWORD PTR[ebp-8], 0 
         je _FilterPixel_AddValue ; quando index == 0, nao precisa configurar nada, pois valor do index zero ja esta em eax
@@ -105,7 +107,7 @@ include clamp.inc
             mov eax, [ecx][2]
             jmp _FilterPixel_AddValue
 
-        ;Add value to color and clamp -----------
+        ; Soma valor e depois clampa entre [0, 255] -----------
         _FilterPixel_AddValue:
             xor ah, ah ; ah = 0
             add eax, DWORD PTR[ebp-4]
@@ -119,7 +121,7 @@ include clamp.inc
         ;----------- Pegando valor no endereco e modificando --------------
         cmp DWORD PTR[ebp-8], 0
         je Filter_Blue
-        cmp DWORD PTR[ebp-8], 1 ; Comparando index
+        cmp DWORD PTR[ebp-8], 1 ; Comparando index para saber qual cor filtrar
         je Filter_Green
         cmp DWORD PTR[ebp-8], 2 ; 
         jmp Filter_Red
@@ -156,18 +158,36 @@ include clamp.inc
         pop ebp
         ret 12; remove parametros da função
 
-    ;Filtra a imagem
+    ;Filtra a imagem, aplicando a logica em cada pixel
     _FilterImage:
         ;Prologo da subrotina --------
         push ebp
         mov ebp, esp
+        sub esp, 12
+
+        ;valor a adicionar
+        mov eax, DWORD PTR[ebp+8]
+        mov DWORD PTR[ebp-4], eax
+
+        ;index para operar
+        mov eax, DWORD PTR[ebp+12]
+        mov DWORD PTR[ebp-8], eax
+
+        ;endereço brg
+        mov eax, DWORD PTR[ebp+16]
+        mov DWORD PTR[ebp-12], eax
               
         _FilterImage_Loop:
             invoke ReadFile, original_fileHandle, addr bgr_color_buffer, 3, addr original_readCount, NULL ;Ler banda BGR
 
-            push offset bgr_color_buffer
-            push 0 ; index
-            push 50 ; valor pra add
+            ;push offset bgr_color_buffer; DWORD PTR[ebp-12] ; endereco
+            ;push 0 ; index
+            ;push value_to_add ; valor
+            ;call _FilterPixel
+
+            push DWORD PTR[ebp-12] ; endereco array BGR
+            push DWORD PTR[ebp-8] ; index
+            push DWORD PTR[ebp-4] ; valor pra add
             call _FilterPixel
              
             invoke WriteFile, copy_fileHandle, addr bgr_color_buffer, 3, addr copy_writeCount, NULL ; Escreve banda BGR no arquivo         
@@ -184,18 +204,81 @@ include clamp.inc
             ;Epilogo da subrotina --------
             mov esp, ebp
             pop ebp
-            ret; desaloca parametros
+            ret 12; desaloca parametros
+
+    ;Le os dados de entrada para aplicar no programa
+    _ReadInputs:
+        ;Prologo da subrotina --------
+        push ebp
+        mov ebp, esp
+              
+        call _MyIO_Setup ; Setup dos handler de entrada e saida do console
+        
+        ;Solicita nome do arquivo original -----------------
+            ;---- Printa mensagem ----
+            push offset CONSOLE_MSG_NOME_ARQUIVO_ORIGINAL
+            call _MyIO_LogMessage
+
+            ;---- Aguarda e trata entrada ----
+            push sizeof originalfile_name
+            push offset originalfile_name
+            call _MyIO_ReadString
+            
+        ;Solicita nome do arquivo copia -----------------
+            ;---- Printa mensagem ----
+            push offset CONSOLE_MSG_NOME_ARQUIVO_COPIA
+            call _MyIO_LogMessage
+
+            ;---- Aguarda e trata entrada ----
+            push sizeof copy_name
+            push offset copy_name
+            call _MyIO_ReadString
+
+        ;Solicitar o index da cor ------------------
+            ;---- Printa mensagem ----
+            push offset CONSOLE_MSG_INDEX_COR
+            call _MyIO_LogMessage
+    
+            ;---- Aguarda e trata entrada ----
+            call _MyIO_ReadInteger
+            mov color_index, eax
+    
+        ;Solicitar valor para adicionar ------------
+            ;---- Printa mensagem ----
+            push offset CONSOLE_MSG_QTD_ADD
+            call _MyIO_LogMessage
+    
+            ;---- Aguarda e trata entrada ----
+            call _MyIO_ReadInteger
+            mov value_to_add, eax
+
+        ;Epilogo da subrotina --------
+        mov esp, ebp
+        pop ebp
+        ret; desaloca parametros
+
         
 start:
-    ; Abre arquivo original em depois solicita modo leitura ----------
-    invoke CreateFile, addr original_fileName, GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL
-    mov original_fileHandle, eax    
+    call _ReadInputs ; Recebe e trata as entradas, salvando-as na memoria
 
-    ; Abre arquivo copia em modo escrever ----------
-    invoke CreateFile, addr copy_fileName, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL
+    ; Abre foto original e depois solicita modo leitura ----------
+    invoke CreateFile, addr originalfile_name, GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL
+    mov original_fileHandle, eax   
+
+    ; Abre foto copia e solicita modo escrever ----------
+    invoke CreateFile, addr copy_name, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL
     mov copy_fileHandle, eax
 
+    push color_index
+    push value_to_add    
     call _CopyFirst54Bytes
+    pop value_to_add
+    pop color_index  
+
+    push offset bgr_color_buffer
+    push color_index
+    push value_to_add  
+
     call _FilterImage
 
     _Exit_Program:
